439fa39c5c9f4753ba5b22391f1566148c449d00
Vinny.sl <v.lima@universidademarketplace.com.br>
2026-05-26 08:52:47 -0300

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -3,4 +3,10 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist',
+    rollupOptions: {
+      input: './index.html'
+    }
+  }
 })
